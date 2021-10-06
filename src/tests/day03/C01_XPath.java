2daf4d75ace35a6dd241a39cb6b0e1784a3d0f8f
mhmmtyksl <mhmmt.yksl1@gmail.com>
2021-10-06 21:03:13 +0200

    ilk

---
+++ src/tests/day03/C01_XPath.java
@@ -0,0 +1,11 @@
+package tests.day03;
+
+public class C01_XPath {
+    public static void main(String[] args) {
+        // 1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        // 2- Add Element butonuna basin
+        // 3- Delete butonu’nun gorunur oldugunu test edin
+        // 4- Delete tusuna basin
+        // 5- Delete butonunun gorunur olmadigini test edin
+    }
+}
